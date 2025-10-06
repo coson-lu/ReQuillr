3483a284f9a1193954ea70523756f1f335c7dabb
Coson Lu <86427638+coson-lu@users.noreply.github.com>
2025-10-06 16:30:25 -0700

    made log slightly more appealing to look at

--- src/app/logviewer.tsx
+++ src/app/logviewer.tsx
@@ -1,18 +1,22 @@
 "use client"
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useLogStore } from "@/stores/useLogStores";
 
 export default function LogViewer() {
   const logs = useLogStore((s) => s.logs);
 
-  // console.log(logs)
 
   return (
     <div className="">
-      <div className="flex flex-col gap-4 w-screen h-[90vh] items-center mt-[5vh] overflow-y-scroll">
+      <div
+        className="mask-t-from-50% mask-b-from-50% flex flex-col w-screen h-[90vh] items-center mt-[5vh] overflow-y-scroll [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100/0"
+      >
         {logs.slice().reverse().map((log, index) => (
-          <div key={index} className="px-10 py-5 w-[50vw] h-25 rounded-md shadow-sm text-[#111] bg-white/90 border border-black/5 transition-transform transform hover:-translate-y-1 hover:shadow-md">
+          <div
+            key={index}
+            className="px-10 py-5 my-2 w-[50vw] h-25 rounded-md shadow-sm text-[#111] bg-white/90 border border-black/5 transition-transform transform hover:-translate-y-1 hover:shadow-md"
+          >
             {log.author}
             {logs.length - index}
           </div>
